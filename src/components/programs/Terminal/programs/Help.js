e741e41eddadec01d422c5c7c97c04d2b4195a49
Marcin Konopka <marcin.konopka98@protonmail.com>
2024-03-02 23:17:34 +0100

    Help update

--- src/components/programs/Terminal/programs/Help.js
+++ src/components/programs/Terminal/programs/Help.js
@@ -10,6 +10,13 @@ class Help extends Program {
       output("   -ping\t - checks the connection to given host");
       output("   -node\t - runs JS code");
       output("   -neofetch\t - prints system specification");
+      output("   -cd\t\t - changes directory");
+      output(
+        "   -cat\t\t - concatenates files and prints on the standard output"
+      );
+      output("   -ls\t\t - lists directory contents");
+      output("   -mkdir\t - makes directories");
+      output("   -touch\t - creates files");
       exit();
     };
   }
